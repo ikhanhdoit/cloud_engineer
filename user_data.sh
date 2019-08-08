dbe8730a8478aa50e4af2fb24ee43284aaebdaf3
Khanh Tran <41774337+ikhanhdoit@users.noreply.github.com>
2019-08-08 16:33:32 -0700

    Update user_data.sh

--- user_data.sh
+++ user_data.sh
@@ -5,4 +5,5 @@ sudo systemctl start httpd
 sudo chkconfig httpd on
 sudo yum install -y vim
 sudo yum install -y mysql
+sudo yum install -y wget
 echo "Fortune-Of-The-Day Coming Soon!" > /var/www/html/index.html
