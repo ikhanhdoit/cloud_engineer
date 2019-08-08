5ab534765548e889b3daf10d005b4227345927c4
Khanh Tran <41774337+ikhanhdoit@users.noreply.github.com>
2019-08-08 16:29:26 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,26 +1,20 @@
-<?php
-$servername = "[RDS endpoint]";
-$username = "[RDS username]";
-$password = "[RDS password]";
-$dbname = "[MySQL database name]";
+<html>
+<body>
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-}
+<h1>Welcome to your Fortune of the Day!</h1>
+<p>Here is your fortune:</p>
 
-$mysql = "SELECT [row] FROM [db table name] ORDER BY rand() LIMIT 1";
+<?php include ('query.php'); ?>
 
-echo '<span style ="font-size: 20px;">Here is your Fortune of the Day:';
-echo '<br/>';
-echo '<br/>';
+<br/>
+<br/>
+<br/>
+</body>
 
+        <form action="insert.php" method="post">
+                Or input your own fortune : <input type="text" name="fortune">
+                <br/>
+                <input type="submit" value="Insert">
+        </form>
 
-foreach ($conn->query($mysql) as $row) {
-        print $row['[MySQL database name]'];
-}
-
-$conn->close();
-?>
+</html>
